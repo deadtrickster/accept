68320c78dd0345ece8f63f377b0cf7ac0a15b624
Ilya Khaprov <dead.trickster@gmail.com>
2016-10-20 12:23:55 +0300

    mix.exs

---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule Accept.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :accept,
+     version: "0.1.0",
+     description: description,
+     package: package]
+  end
+
+  defp description do
+    """
+    Accept header(s) for Erlang/Elixir.
+    """
+  end
+
+  defp package do
+    [build_tools: ["rebar3"],
+     maintainers: ["Ilya Khaprov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/deadtrickster/accept"},
+     files: ["bin", "src", "include", "README.md", "LICENSE", "rebar.config"]]
+  end
+end
